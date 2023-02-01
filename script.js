58fe630a8460731384b6f63fcf2da363116ca961
Leonardo Queiroz <leonardobcq@outlook.com>
2023-02-01 10:07:53 -0300

    Create all buttons and event listeners Create and link CSS file

--- script.js
+++ script.js
@@ -1,28 +1,64 @@
-const rock = document.querySelector("#rock");
-rock.addEventListener("click", playRound("rock", getComputerChoice()));
-const paper = document.querySelector("#paper");
-paper.addEventListener("click", playRound("paper", getComputerChoice()));
-const scissors = document.querySelector("#scissors");
-scissors.addEventListener("click", playRound("scissors", getComputerChoice()));
-
+const scoreDiv = document.querySelector("#score");
 const resultsDiv = document.querySelector("#results");
+const resetDiv = document.querySelector("#reset");
+const resetMsg = document.querySelector("#reset-message");
+const playAgainBtn = document.querySelector("#play-again");
+playAgainBtn.addEventListener("click", function() {
+   resetGame();    
+})
+const notAgainBtn = document.querySelector("#not-again");
+notAgainBtn.addEventListener("click", function() {
+    resetDiv.classList.add("hidden");
+});
 const roundResults = document.createElement("p");
 roundResults.setAttribute("id", "round-results");
-const gameResults = document.createElement("p");
-gameResults.setAttribute("id", "game-results");
 const finalResults = document.createElement("p");
 finalResults.setAttribute("id", "final-results");
-
+const winScore = document.querySelector("#score-win");
+const lossScore = document.querySelector("#score-loss");
+const tieScore = document.querySelector("#score-tie");
 
 let winCount = 0;
-let tieCount = 0;
 let lossCount = 0;
+let tieCount = 0;
 
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", function() {
+    playRound("rock", getComputerChoice())
+});
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", function() {
+    playRound("paper", getComputerChoice())
+});
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", function() {
+    playRound("scissors", getComputerChoice())
+});
 
 function getComputerChoice() {
     const options = ["rock", "paper", "scissors"];
     const randomChoice = Math.floor(Math.random() * options.length);    
     return options[randomChoice];
+}
+
+function renderScore() {
+    winScore.textContent = winCount;
+    lossScore.textContent = lossCount;
+    tieScore.textContent = tieCount;
+}
+
+function resetGame() {
+    winCount = 0;
+    lossCount = 0;
+    tieCount = 0;
+    renderScore();
+    rock.disabled = false;
+    paper.disabled = false;
+    scissors.disabled = false;
+    resetMsg.textContent = "";
+    roundResults.textContent = "";
+    finalResults.textContent = "";
+    resetDiv.classList.add("hidden");
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -31,14 +67,17 @@ function playRound(playerSelection, computerSelection) {
             roundResults.textContent = "It's a Tie! I also picked Rock.";
             resultsDiv.appendChild(roundResults);
             tieCount++;
+            renderScore();            
         } else if (computerSelection === "paper") {
             roundResults.textContent = "You Lose! Rock loses to Paper.";
             resultsDiv.appendChild(roundResults);
-            lossCount++;           
+            lossCount++;
+            renderScore();           
         } else if (computerSelection === "scissors") {
             roundResults.textContent = "You Win!! Rock beats Scissors!!";
             resultsDiv.appendChild(roundResults);
             winCount++;
+            renderScore();
         }
 
     } else if (playerSelection === "paper") {
@@ -46,82 +85,53 @@ function playRound(playerSelection, computerSelection) {
             roundResults.textContent = "You Win!! Paper beats Rock!!";
             resultsDiv.appendChild(roundResults);
             winCount++;
+            renderScore();
         } else if (computerSelection === "paper") {
             roundResults.textContent = "It's a Tie! I also picked Paper.";
             resultsDiv.appendChild(roundResults);
-            tieCount++;        
+            tieCount++;
+            renderScore();        
         } else if (computerSelection === "scissors") {
             roundResults.textContent = "You Lose! Paper loses to Rock.";
             resultsDiv.appendChild(roundResults);
-            lossCount++;   
+            lossCount++;
+            renderScore();   
         }
 
     } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
             roundResults.textContent = "You Lose! Scissors loses to Rock.";
             resultsDiv.appendChild(roundResults);
-            lossCount++;   
+            lossCount++;
+            renderScore();   
         } else if (computerSelection === "paper") {
             roundResults.textContent = "You Win!! Scissors beats Paper!!";
             resultsDiv.appendChild(roundResults);
             winCount++;
+            renderScore();
         } else if (computerSelection === "scissors") {
             roundResults.textContent = "It's a Tie! I also picked Rock.";
             resultsDiv.appendChild(roundResults);
             tieCount++;
+            renderScore();
         }
     }
-}
 
-function game() {
-    let winCount = 0;
-    let tieCount = 0;
-    let lossCount = 0;
-    let round;
-    let playAgain;
-
-    /* for (let i = 0; i < 5; i++) {
-        playerChoice = (prompt("Rock, Paper or Scissors?")).toLowerCase();
-        while(!validChoice(playerChoice)) {
-            alert("You didn't pick rock, paper or scissors!");
-            playerChoice = (prompt("Rock, Paper or Scissors?")).toLowerCase();            
-        }
-        round = playRound(playerChoice, getComputerChoice());
-        if (round === "win") {
-            winCount++;
-        } else if (round === "tie") {
-            tieCount++;
-        } else if (round === "loss") {
-            lossCount++;
-        }
-    } */
-
-    alert(`Here's how our game went:\n- You won ${winCount} round(s)\n`+
-    `- You lost ${lossCount} round(s)\n- We tied ${tieCount} round(s)`);
-    console.log(`Here's how our game went:\n- You won ${winCount} round(s)\n`+
-    `- You lost ${lossCount} round(s)\n- We tied ${tieCount} round(s)`);
-
-    if (winCount > lossCount) {
-        alert("****************\n\n       You Win!!       \n\n****************");
-        console.log("****************\n\n       You Win!!       \n\n****************");
-        playAgain = confirm("Give me another chance and I'll beat you (¬､¬)");
-        if (playAgain) {
-            game();
-        }
-
-    } else if (lossCount > winCount) {
-        alert("XxXxXxXxXxXxXxX\n\n       You Lose!       \n\nXxXxXxXxXxXxXxX");
-        console.log("XxXxXxXxXxXxXxX\n\n       You Lose!       \n\nXxXxXxXxXxXxXxX");
-        playAgain = confirm("Would you like to try again? ;)");
-        if (playAgain) {
-            game();
-        }
-    } else if (winCount == lossCount) {
-        alert("*#*#*#*#*#*#*#*\n\nWe Tied!  ¯\\_(ツ)_/¯\n\n*#*#*#*#*#*#*#*");
-        console.log("*#*#*#*#*#*#*#*\n\nWe Tied!  ¯\\_(ツ)_/¯\n\n*#*#*#*#*#*#*#*");
-        playAgain = confirm("Would you like to play again?");
-        if (playAgain) {
-            game();
-        }
-    }    
+    if (winCount === 5) {
+        finalResults.textContent = "****************       You Win!!       ****************";
+        resultsDiv.appendChild(finalResults);
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+        resetMsg.textContent = "Would you like to play again? I'm going to beat you for sure next time!"
+        resetDiv.classList.remove("hidden");      
+    } else if (lossCount === 5) {
+        finalResults.textContent = "XxXxXxXxXxXxXxX       You Lose!       XxXxXxXxXxXxXxX";
+        resultsDiv.appendChild(finalResults);
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+        resetMsg.textContent = "Would you like to try one more time? I'll do my best to go easy on you this time."
+        resetDiv.classList.remove("hidden");        
+    }
 }
